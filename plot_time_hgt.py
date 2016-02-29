97b9908f60b549c78b65defe71e8a54b007f16b6
rvalenzuelar <raul.valenzuela@colorado.edu>
2016-02-29 13:43:45 -0700

    pep8 formatted

--- plot_time_hgt.py
+++ plot_time_hgt.py
@@ -5,41 +5,41 @@ from matplotlib.backends.backend_pdf import PdfPages
 
 reload(wp)
 
-case=[8]
+case = [8]
 # case=range(8,15)
-res='coarse'
+res = 'coarse'
 
-o='case{}_total_wind_{}.pdf'
-t='Total wind speed - {} resolution - Case {}'
+o = 'case{}_total_wind_{}.pdf'
+t = 'Total wind speed - {} resolution - Case {}'
 for c in case:
 
-	''' creates plot with seaborn style '''
-	with sns.axes_style("white"):
-		f, ax1 = plt.subplots(figsize=(11,8.5))	
+    ''' creates plot with seaborn style '''
+    with sns.axes_style("white"):
+        f, ax1 = plt.subplots(figsize=(11, 8.5))
 
-	wspd,wdir,time,hgt = wp.make_arrays(resolution=res,
-											   surface=True,
-											   case=str(c),
-											   period=False)
+    wspd, wdir, time, hgt = wp.make_arrays(resolution=res,
+                                           surface=True,
+                                           case=str(c),
+                                           period=False)
 
-	# wp.plot_time_height(ax=ax1, wspd=wspd, time=time, 
-	# 					height=hgt, vrange=[0,25],
-	# 					cname='YlGnBu_r',title='Total wind speed')
-	# wp.add_windstaff(wspd,wdir,time,hgt,ax=ax1, color=(1,0.2,0))
-	# wp.add_soundingTH('bvf_moist',str(c),ax=ax1,wptime=time,wphgt=hgt,sigma=1)
+    # wp.plot_time_height(ax=ax1, wspd=wspd, time=time,
+    #                   height=hgt, vrange=[0,25],
+    #                   cname='YlGnBu_r',title='Total wind speed')
+    # wp.add_windstaff(wspd,wdir,time,hgt,ax=ax1, color=(1,0.2,0))
+    # wp.add_soundingTH('bvf_moist',str(c),ax=ax1,wptime=time,wphgt=hgt,sigma=1)
 
+    wp.plot_colored_staff(ax=ax1, wspd=wspd, wdir=wdir, time=time,
+                          height=hgt, spd_range=[0, 20], spd_delta=2,
+                          vdensity=1, hdensity=1, cmap='nipy_spectral',
+                          title=t.format(res.title(), str(c).zfill(2)))
+    wp.add_soundingTH('bvf_moist', str(c), ax=ax1,
+                      wptime=time, wphgt=hgt, sigma=1)
 
-	wp.plot_colored_staff(ax=ax1, wspd=wspd, wdir=wdir, time=time, 
-						height=hgt, spd_range=[0,20], spd_delta=2,
-						vdensity=1, hdensity=1,	cmap='nipy_spectral',
-						title=t.format(res.title(), str(c).zfill(2)))
-	wp.add_soundingTH('bvf_moist',str(c),ax=ax1,wptime=time,wphgt=hgt,sigma=1)
+    # savename=o.format(str(c).zfill(2), res)
+    # wp_pdf=PdfPages(savename)
+    # wp_pdf.savefig()
+    # wp_pdf.close()
 
-	# savename=o.format(str(c).zfill(2), res)
-	# wp_pdf=PdfPages(savename)
-	# wp_pdf.savefig()
-	# wp_pdf.close()
-
-	plt.show(block=False)
-	# plt.show()
-	# plt.close('all')
+    plt.show(block=False)
+    # plt.show()
+    # plt.close('all')
